65b7641b8a31bd0204c5fdd7d703cf228c63ff8b
Gamy Malasarte <gmalasarte@Gamys-Mac.local>
2017-07-06 23:12:29 -0700

    asdf

---
+++ SidebarMenu/HomeTableViewController.swift
@@ -0,0 +1,78 @@
+//
+//  HomeTableViewController.swift
+//  SidebarMenu
+//
+//  Created by Simon Ng on 2/2/15.
+//  Copyright (c) 2015 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewController: UITableViewController {
+    @IBOutlet var menuButton:UIBarButtonItem!
+    @IBOutlet var extraButton:UIBarButtonItem!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if revealViewController() != nil {
+//            revealViewController().rearViewRevealWidth = 62
+            menuButton.target = revealViewController()
+            menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+
+            revealViewController().rightViewRevealWidth = 150
+            extraButton.target = revealViewController()
+            extraButton.action = #selector(SWRevealViewController.rightRevealToggle(_:))
+
+            view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+            
+        
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // Return the number of sections.
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of rows in the section.
+        return 3
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! HomeTableViewCell
+
+        // Configure the cell...
+        if indexPath.row == 0 {
+            cell.postImageView.image = UIImage(named: "watchkit-intro")
+            cell.postTitleLabel.text = "WatchKit Introduction: Building a Simple Guess Game"
+            cell.authorLabel.text = "Simon Ng"
+            cell.authorImageView.image = UIImage(named: "author")
+
+        } else if indexPath.row == 1 {
+            cell.postImageView.image = UIImage(named: "custom-segue-featured-1024")
+            cell.postTitleLabel.text = "Building a Chat App in Swift Using Multipeer Connectivity Framework"
+            cell.authorLabel.text = "Gabriel Theodoropoulos"
+            cell.authorImageView.image = UIImage(named: "appcoda-300")
+            
+        } else {
+            cell.postImageView.image = UIImage(named: "webkit-featured")
+            cell.postTitleLabel.text = "A Beginner’s Guide to Animated Custom Segues in iOS 8"
+            cell.authorLabel.text = "Gabriel Theodoropoulos"
+            cell.authorImageView.image = UIImage(named: "appcoda-300")
+            
+        }
+
+        return cell
+    }
+    
+}
